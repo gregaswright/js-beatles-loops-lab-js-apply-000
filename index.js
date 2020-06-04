17754ae7eb68e631c9730471de8bede3895a32fe
Gregory A.S. Wright <geslim@gmail.com>
2020-06-04 20:31:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(array){
   var i = 0;
-  while (i < array.length) {
+  while (i > array.length) {
     return `${array[i]}!!!`
     array++
   }
